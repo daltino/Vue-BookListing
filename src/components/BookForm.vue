830becc90c463dcc92e6a90a6a7dca0cb37aa9e1
Dalton Whyte <daltino06@yahoo.com>
2020-04-12 20:36:12 +0200

    Add a BookForm component to add new books

---
+++ src/components/BookForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form class="" action="#" v-on="submit.prevent" method="post">
+    <input type="text" name="title" v-model="bookTitle" value="" placeholder="Book Title">
+    <input type="text" name="author" v-model="bookAuthor" value="" placeholder="Book Author">
+    <button @submit="bookSubmit(bookTitle, bookAuthor)" type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'BookForm',
+  props: ['books'],
+  data: function() {
+    return {
+      bookTitle: '',
+      bookAuthor: '',
+    }
+  },
+  methods: {
+    bookSubmit: function(bookTitle, bookAuthor) {
+      this.$emit('addBook', bookTitle, bookAuthor);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
