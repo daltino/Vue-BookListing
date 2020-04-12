ada204213b70de3dd65c6e6eac4bacbd11f7cb7c
Dalton Whyte <daltino06@yahoo.com>
2020-04-12 19:44:46 +0200

    Created BookList component

---
+++ src/components/BookList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div><h1>{{title}}</h1></div>
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  data: function() {
+    return {
+      title: 'All Books',
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
